215446df80c6f53ef305d779ace566812b69d771
erickbarbosa <erick.barbosa-ext@viavarejo.com.br>
2023-11-21 17:06:14 -0300

    feat: created TaskService

---
+++ src/main/java/com/book/task/service/TaskService.java
@@ -0,0 +1,35 @@
+package com.book.task.service;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.book.task.dto.TaskDto;
+import com.book.task.entity.Task;
+import com.book.task.repository.TaskRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class TaskService {
+
+	private TaskRepository repository;
+	
+	public TaskService(TaskRepository repository) {
+		this.repository = repository;
+	}
+	
+	public Task get(long taskId) {
+		Optional<Task> task = repository.findById(taskId);
+		return task.get();
+	}
+	
+	public Task saveTask(TaskDto taskDto) {
+		ModelMapper modelMapper = new ModelMapper();
+		Task task = modelMapper.map(taskDto, Task.class);
+		
+		return repository.save(task);
+	}
+}
